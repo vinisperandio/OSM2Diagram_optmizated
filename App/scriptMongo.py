7579bfc018c2b81a5dd4ac009e6fe6ebf87aa768
vinisperandio <vinisperandio13@gmail.com>
2018-10-09 20:37:09 -0300

    GeoJson

--- App/scriptMongo.py
+++ App/scriptMongo.py
@@ -1,8 +1,5 @@
 
 def coordinates(listAll, i):
-    scriptStereotype = ""
-    scriptCoordinates = ""
-    script = ""
     charStereotypeOpen = ["", "[ ", "[ [ "]
     charStereotypeClose = ["", " ]", " ] ]"]
     stereotypeFLG = 0
@@ -22,15 +19,15 @@ def coordinates(listAll, i):
         stereotype = "Polygon"
         stereotypeFLG = 2
 
-    scriptCoordinates = " coordinates: " + charStereotypeOpen[stereotypeFLG]
+    scriptCoordinates = " \"coordinates\": " + charStereotypeOpen[stereotypeFLG]
     flgLat = listAll[i]['lat0']
     flgLon = listAll[i]['lon0']
     scriptCoordinates += " [ "
-    scriptCoordinates += str(listAll[i]['lat0']) + ", " + str(listAll[i]['lon0'])
+    scriptCoordinates += str(listAll[i]['lon0']) + ", " + str(listAll[i]['lat0'])
     scriptCoordinates += " ] "
     while 'lat' + str(num) in listAll[i].keys():
         scriptCoordinates += charVirgula[flgVirgula] +" [ "
-        scriptCoordinates += str(listAll[i]['lat' + str(num)]) + ", " + str(listAll[i]['lon' + str(num)])
+        scriptCoordinates += str(listAll[i]['lon' + str(num)]) + ", " + str(listAll[i]['lat' + str(num)])
         scriptCoordinates += " ] "
         flgVirgula = 0
         if flgLat == listAll[i]['lat'+str(num)] and flgLon == listAll[i]['lon'+str(num)]:
@@ -43,8 +40,13 @@ def coordinates(listAll, i):
 
         num += 1
 
+    if flgMulti != "":
+        chr = scriptCoordinates.find("[")
+        scriptCoordinates = scriptCoordinates[:chr] + "[" + scriptCoordinates[chr:]
+        scriptCoordinates += "]"
+
     stereotype = flgMulti + stereotype
-    scriptStereotype = "geometries: { type: " + "\"" + stereotype + "\","
+    scriptStereotype = "\"type\": \"Feature\",\"geometry\": { \"type\": " + "\"" + stereotype + "\","
     script = scriptStereotype + scriptCoordinates
     script += charStereotypeClose[stereotypeFLG] + " }})\n"
 
@@ -57,24 +59,25 @@ def othersAtrib(listAll, i):
     remove = ["name","stereotype","amenity","highway","shop","building"]
     for j in listAll[i].keys():
         if j not in remove and "lat" not in j and "lon" not in j :
-            script += j + " : \"" + listAll[i][j] + "\", "
+            script += "\"" + j + "\"" + " : \"" + listAll[i][j] + "\", "
 
     return script.replace("addr:", "")
 
 def openJSON(listScript):
     dotA = listScript.find('.') + 1
     dotB = listScript.find('.', dotA)
-    arqJSON = open("Resultado/" + str(listScript[dotA:dotB]) + ".json", 'w+')
+    arqJSON = open("Resultado/" + str(listScript[dotA:dotB]) + ".geojson", 'w+')
 
     return arqJSON
 
 def scriptGeneration(listAll, mapName):
     arqScript = open("Resultado/script.txt", 'w+')
     listScript = []
-    script = ""
+    scriptInsert = ""
+    scriptJson = ""
 
     for i in range(len(listAll)):
-
+        #print(listAll[i])
         if "amenity" in listAll[i]:
             script = "db."+listAll[i]['amenity']+".insert({ "
 
@@ -86,39 +89,41 @@ def scriptGeneration(listAll, mapName):
 
         elif "building" in listAll[i]:
             script = "db."+listAll[i]['building']+".insert({ "
-
+        else:
+            script = ""
 
         if "name" in listAll[i]:
-            script += "name: \""+ listAll[i]['name'] + "\", "
+            script += "\"name\": \""+ listAll[i]['name'] + "\", "
 
         script += othersAtrib(listAll, i)
         script += coordinates(listAll, i)
         listScript.append(script)
 
-
     listScript.sort()
     arqScript.write("use "+ mapName +"\n\n")
     arrabN = listScript[0].split(".")[1]
     arqJSON = openJSON(listScript[0])
+    scriptJson = "{\"type\": \"FeatureCollection\", \"features\": [\n"
     for x in listScript:
 
         if x.split(".")[1] != arrabN:
+            scriptJson = scriptJson[:-2] + "\n"
+            scriptJson += "]}"
+            arqJSON.write(scriptJson)
             arqJSON.close()
             arqJSON = openJSON(x)
-            arqScript.write("\n")
+            scriptJson = "{\"type\": \"FeatureCollection\", \"features\": [\n"
             arrabN = x.split(".")[1]
+            arqScript.write("\n")
 
         arqScript.write(x)
-        arqJSON.write(x[x.find('{'):x.find(')')]+"\n")
+        scriptJson += x[x.find('{'):x.rfind(')')]+",\n"
 
     arqScript.close()
+    scriptJson = scriptJson[:-2] + "\n"
+    scriptJson += "]}"
+    arqJSON.write(scriptJson)
     arqJSON.close()
 
     print("Script Table completed")
-    # asd = listScript[0].find('.')
-    # print(asd)
-    # print(listScript[0].find('.', asd+1))
-    # print(listScript[0][3:11])
-    # print(listScript[0].find('{'))
-    # print(listScript[0][19:])
     return
