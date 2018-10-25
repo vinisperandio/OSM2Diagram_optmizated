ebfdbaf85a3ec3c733c225db00b64fb70394ccd0
vinisperandio <vinisperandio13@gmail.com>
2018-10-25 13:33:33 -0300

    Minimundo completo remoção de SOURCE Inserção de objetos sem nome

--- App/XMLSoup.py
+++ App/XMLSoup.py
@@ -4,6 +4,7 @@ import time
 import scriptMongo
 import sys
 
+
 dicElements = {}  # responsavel por separar as caracteristicas (name, coordinates...) dentro da funcao Find_tag_coord
 dicWay = {}    # dicionario das tags Way
 listWay = []   # lista que recebe todos os blocos XML da tag Way
