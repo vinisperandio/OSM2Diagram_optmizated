c4539e1afcdda4a88714946f222074a5ca3ba4e5
vinisperandio <vinisperandio13@gmail.com>
2019-01-06 16:01:03 -0200

    jf error shp

--- App/XMLSoup.py
+++ App/XMLSoup.py
@@ -195,7 +195,7 @@ print(listAllEntities)
 #### GERAR SCRIP TABELAS
 fileName = xml_file.name
 listNames = scriptMongo.scriptGeneration(listAllEntities, fileName[4:])
-# listNames = sorted(set(listNames))
+listNames = sorted(set(listNames))
 
 # print("Number of entity:" + str(len(listNames)))
 
@@ -207,7 +207,7 @@ point=0
 
 for i in listNames:
     with open("Resultado/"+ i +".geojson", encoding='windows-1252') as file: #, encoding='windows-1252'
-        arq = json.load(file)
+        arq = json.load(file, strict=False)
     data = json.dumps(arq)
 
     linestring = data.count('LineString')
